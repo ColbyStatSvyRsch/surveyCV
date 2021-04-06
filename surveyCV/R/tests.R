20b27e637f0cf42c79ea1f6a31e4d6e8c75cb104
Cole Guerin <cjguer21@colby.edu>
2021-04-05 23:44:34 -0400

    Tested  some examples for motivating example

--- surveyCV/R/tests.R
+++ surveyCV/R/tests.R
@@ -1,4 +1,23 @@
 if(FALSE) {
   NSFG_data <- read.csv("data-raw/NSFG_data.csv")
-  plot(NSFG_data$income, NSFG_data$PregNum)
+  NSFG_data$strata <- as.factor(NSFG_data$strata)
+  NSFG_data$SECU <- as.factor(NSFG_data$SECU)
+  library(ggplot2)
+  ggplot(NSFG_data, aes(x = YrEdu, y = income)) +
+    geom_jitter() +
+    geom_smooth(method = "loess", se = TRUE) +
+    facet_wrap(SECU~., ncol = 2)
+  ggplot(NSFG_data, aes(x = YrEdu, y = income)) +
+    geom_jitter() +
+    geom_smooth(method = "loess", se = TRUE) +
+    facet_wrap(strata~., ncol = 2)
+  cv.svy(NSFG_data, "income~YrEdu", nfolds = 4, strataID = "strata",
+         clusterID = "SECU", nest = TRUE, weights = "wgt", N = 5100, afolds = FALSE)
+  cv.svy(NSFG_data, "income~YrEdu", nfolds = 4, strataID = "strata",
+         clusterID = "SECU", nest = TRUE, weights = "wgt", N = 5100, afolds = TRUE)
+  
+  cv.svy(NSFG_data, "income~race", nfolds = 4, strataID = "strata",
+         clusterID = "SECU", nest = TRUE, weights = "wgt", N = 5100, afolds = FALSE)
+  cv.svy(NSFG_data, "income~race", nfolds = 4, strataID = "strata",
+         clusterID = "SECU", nest = TRUE, weights = "wgt", N = 5100, afolds = TRUE)
 }
