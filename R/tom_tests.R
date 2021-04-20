921ce7a40db24ab2c866255968b873bf9493cf91
Thomas McMahon <thomasmcmahon@Thomass-MacBook-Pro.local>
2021-04-19 20:26:30 -0400

    created a plotting function for the significant test cole foun

--- R/tom_tests.R
+++ R/tom_tests.R
@@ -5,6 +5,7 @@ if(FALSE) {
   library(ggplot2)
   library(dplyr)
   library(splines)
+  library(gridExtra)
   ggplot(NSFG_data, aes(x = YrEdu, y = income)) +
     geom_jitter() +
     geom_smooth(method = "loess", se = TRUE) +
@@ -96,4 +97,92 @@ SCNSFG <-NSFG_data[NSFG_data$eduCat=="Some college",]
 
 # not enough PSU
 
+
+##cole found some examples that looked promising (see below) gonna write a plotting function for them
+
+cv.svy(NSFG_data, "income~ns(YrEdu, df = 5)", nfolds = 4, weights = "wgt", N = 5100)
+cv.svy(NSFG_data, "income~ns(YrEdu, df = 5)", nfolds = 4, strataID = "strata",
+       clusterID = "SECU", nest = TRUE, weights = "wgt", N = 5100)
+
+cv.svy(NSFG_data, "income~race", nfolds = 4, weights = "wgt", N = 5100)
+cv.svy(NSFG_data, "income~race", nfolds = 4, strataID = "strata",
+       clusterID = "SECU", nest = TRUE, weights = "wgt", N = 5100)
+
+income.year_edu.plot <- function(loops) {
+  # Making an empty data set for output when we take into consideration the design method
+  method.ds <- data.frame(MSE = c())
+  # Making an empty data set for output when we ignore the design method
+  ignore.ds <- data.frame(MSE = c())
+   # looping and generating MSEs
+  for (i in 1:loops) {
+    set.seed(i)
+    method.data <- cv.svy(NSFG_data, "income~ns(YrEdu, df = 5)", nfolds = 4, strataID = "strata",
+                          clusterID = "SECU", nest = TRUE, weights = "wgt", N = 5100)
+    method.ds2 <- data.frame(df = 5, MSE = method.data[,1])
+    method.ds <- rbind(method.ds, method.ds2)
+    ignore.data <- cv.svy(NSFG_data, "income~ns(YrEdu, df = 5)", nfolds = 4, weights = "wgt", N = 5100)
+    ignore.ds2 <- data.frame(df = 5, MSE = ignore.data[,1])
+    ignore.ds <- rbind(ignore.ds, ignore.ds2)
+  }
+  ignore.ds$df <- as.factor(ignore.ds$df)
+  method.ds$df <- as.factor(method.ds$df)
+
+  plot1 <- ggplot(data = ignore.ds, mapping = aes(x = df, y = MSE)) +
+    geom_boxplot() +
+    ggtitle("MSEs When Ignoring Survey Design") +
+    ylim(18000, 19000)
+  # Making a ggplot object for the MSE spread comparison
+  plot2 <- ggplot(data = method.ds, mapping = aes(x = df, y = MSE)) +
+    geom_boxplot() +
+    ggtitle("MSEs When Taking Into Account the Survey Design") +
+    ylim(18000, 19000)
+
+  # Making a grid display of the two plot objects above
+  grid.arrange(plot1,plot2, ncol = 2)
+
 }
+
+income.race.plot <- function(loops) {
+  # Making an empty data set for output when we take into consideration the design method
+  method.ds <- data.frame(MSE = c())
+  # Making an empty data set for output when we ignore the design method
+  ignore.ds <- data.frame(MSE = c())
+  # looping and generating MSEs
+  for (i in 1:loops) {
+    set.seed(i)
+    method.data <- cv.svy(NSFG_data, "income~race", nfolds = 4, strataID = "strata",
+                          clusterID = "SECU", nest = TRUE, weights = "wgt", N = 5100)
+    method.ds2 <- data.frame(df = 5, MSE = method.data[,1])
+    method.ds <- rbind(method.ds, method.ds2)
+    ignore.data <- cv.svy(NSFG_data, "income~race", nfolds = 4, weights = "wgt", N = 5100)
+    ignore.ds2 <- data.frame(df = 5, MSE = ignore.data[,1])
+    ignore.ds <- rbind(ignore.ds, ignore.ds2)
+  }
+  ignore.ds$df <- as.factor(ignore.ds$df)
+  method.ds$df <- as.factor(method.ds$df)
+
+  plot1 <- ggplot(data = ignore.ds, mapping = aes(x = df, y = MSE)) +
+    geom_boxplot() +
+    ggtitle("MSEs When Ignoring Survey Design")
+    #ylim(18000, 19000)
+  # Making a ggplot object for the MSE spread comparison
+  plot2 <- ggplot(data = method.ds, mapping = aes(x = df, y = MSE)) +
+    geom_boxplot() +
+    ggtitle("MSEs When Taking Into Account the Survey Design")
+    #ylim(18000, 19000)
+
+  # Making a grid display of the two plot objects above
+  grid.arrange(plot1,plot2, ncol = 2)
+
+}
+# we see a significant difference here
+income.year_edu.plot(loops = 100)
+#not so much here
+income.race.plot(loops = 100)
+
+#we should use the income~year_edu plot for our example in the vignette
+
+
+}
+
+
