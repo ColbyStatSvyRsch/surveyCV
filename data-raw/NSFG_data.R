766139d7202a2f66857ffae93ec62ea007c086d9
Jerzy Wieczorek <jawieczo@colby.edu>
2021-07-12 15:07:33 -0400

    Move knitted vignette into a new Rbuildignore'd folder

--- data-raw/NSFG_data.R
+++ data-raw/NSFG_data.R
@@ -3,6 +3,3 @@
 
 # TODO: Remove the .csv file from the /data-raw directory
 # and only keep code here.
-
-# TODO: Figure out how to get .Rbuildignore to ignore /data-raw
-# so that R CMD CHECK doesn't complain about it.
