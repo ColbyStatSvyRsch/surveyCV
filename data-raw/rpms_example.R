e0effb1cef8389ceee70f4d643e42984c73dcdca
Jerzy Wieczorek <jawieczo@colby.edu>
2022-03-14 21:36:29 -0400

    Make sure rpms_example.R matches `intro` vignette and rerun to double check the example results

--- data-raw/rpms_example.R
+++ data-raw/rpms_example.R
@@ -18,55 +18,85 @@ library(rpms)
 #### just to make it run faster (1.5 minutes instead of 20 minutes)
 #### though I got very similar results either way
 
+
+
+
+# Based on example("rpms"):
+#   model the mean of retirement account value `IRAX` among households with
+#   reported retirement account values > 0,
+#   predicted from householder education, age, and urban/rural location
+library(rpms)
+data(CE)
+
+# Generate fold IDs that account for clustering in the survey design
+# for the IRAX>0 subset of the CE dataset
 nfolds <- 5
-s1 <- which(CE$IRAX > 0)
-CEsubset <- CE[s1, ]
-nrow(CEsubset) # 4509
-
+CEsubset <- CE[which(CE$IRAX > 0), ]
 CEsubset$.foldID <- folds.svy(CEsubset, nfolds = nfolds, clusterID = "CID")
-names(CEsubset)
-table(CEsubset$.foldID)
-table(CEsubset$.foldID, CEsubset$CID)
 
+#names(CEsubset)
+#table(CEsubset$.foldID)
+#table(CEsubset$.foldID, CEsubset$CID)
+
+# Use CV to tune the bin_size parameter of rpms_forest()
 bin_sizes <- c(10, 20, 50, 100, 250, 500)
+
+# Create placeholder for weighted Sums of Squared Errors
 SSEs <- rep(0, length(bin_sizes))
 
+# With f_size=50 it takes around 2 minutes
 system.time({
-  for(ff in 1:nfolds) {
+  for(ff in 1:nfolds) { # For every fold...
+    # Use .foldID to split data into training and testing sets
     train <- subset(CEsubset, .foldID != ff)
     test  <- subset(CEsubset, .foldID == ff)
-    for(bb in 1:length(bin_sizes)) {
-      rf <- rpms_forest(IRAX ~ EDUCA + AGE + BLS_URBN, 
-                        data = train, weights = ~FINLWT21, clusters = ~CID,
+
+    for(bb in 1:length(bin_sizes)) { # For every value of the tuning parameter...
+      # Fit a new model
+      rf <- rpms_forest(IRAX ~ EDUCA + AGE + BLS_URBN,
+                        data = train,
+                        weights = ~FINLWT21, clusters = ~CID,
                         bin_size = bin_sizes[bb], f_size = 50)
+      # Get predictions and squared errors
       yhat <- predict(rf, newdata = test)
       res2 <- (yhat - test$IRAX)^2
-      # weighted SSEs, not MSEs yet, b/c cluster sizes may differ across folds
+      # Sum up weighted SSEs, not MSEs yet,
+      # b/c cluster sizes may differ across folds and b/c of survey weights
       SSEs[bb] <- SSEs[bb] + sum(res2 * test$FINLWT21)
     }
   }
-  MSEs <- SSEs / sum(CEsubset$FINLWT21)
 })
 
-# Print the results
+# Divide entire weighted sum by the sum of weights
+MSEs <- SSEs / sum(CEsubset$FINLWT21)
+
+# Show results
 cbind(bin_sizes, MSEs)
 
-
-#### From a previous run with default f_size = 500,
-#### instead of the faster f_size=50 above...
-# > cbind(bin_sizes, MSEs)
-# bin_sizes         MSEs
-# [1,]        10 204246617270
-# [2,]        20 202870633392
-# [3,]        50 201393921358
-# [4,]       100 201085838446
-# [5,]       250 201825549231
-# [6,]       500 204155844501
+#> bin_sizes         MSEs
+#> [1,]        10 204246617270
+#> [2,]        20 202870633392
+#> [3,]        50 201393921358
+#> [4,]       100 201085838446
+#> [5,]       250 201825549231
+#> [6,]       500 204155844501
 
 # Bin size 100 had the lowest survey-weighted MSE across the 5 survey CV folds,
 # though sizes 50 and 250 were quite similar;
 # any of these seem reasonable to use for a RF model on the full dataset
 
-rf <- rpms_forest(IRAX ~ EDUCA + AGE + BLS_URBN, 
-                  data = CEsubset, weights = ~FINLWT21, clusters = ~CID,
-                  bin_size = 100)
+# rf <- rpms_forest(IRAX ~ EDUCA + AGE + BLS_URBN,
+#                   data = CEsubset, weights = ~FINLWT21, clusters = ~CID,
+#                   bin_size = 100)
+
+
+#### What about using `folds.svydesign()` instead?
+
+CE.svydes <- svydesign(id = ~CID, weights = ~FINLWT21, data = CEsubset)
+# Use update() to add variables to a svydesign object
+CE.svydes <- update(CE.svydes,
+                    .foldID = folds.svydesign(CE.svydes, nfolds = nfolds))
+# Now the fold IDs are available as CE.svydes$variables$.foldID
+table(CE.svydes$variables$.foldID)
+#>   1   2   3   4   5
+#> 813 923 928 885 960
